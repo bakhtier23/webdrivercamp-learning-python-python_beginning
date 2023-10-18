d27331db8fe654598720e6e2c2641685f698e8c8
Bakhti <jbakhtier@yahoo.com>
2023-10-17 18:08:40 -0700

    Printed a float for 3.14

---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+number = 3.141592653589793238
+print(f"Learning Python is fun!' - {number:.2f} %")
+print("Learning Python is fun!' - %s %%" % round (number, 2))
+print("Learning Python is fun!' - {:.2f} %".format(number))
